66c32dd8044e7da1f0e8733daa1847ca3dc0d6bb
Brian Huynh <brianhuynh1@gmail.com>
2016-03-30 13:21:06 -0700

    Made small change in settings menu

--- GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/SettingsActivity.java
+++ GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/SettingsActivity.java
@@ -1,8 +1,10 @@
 package edu.csumb.gamecontroller;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends Activity {
 
@@ -13,5 +15,9 @@ public class SettingsActivity extends Activity {
 
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+    public void loadTest(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/SettingsFragment.java
+++ GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/SettingsFragment.java
@@ -1,6 +1,7 @@
 package edu.csumb.gamecontroller;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
@@ -19,6 +20,5 @@ public class SettingsFragment extends PreferenceFragment {
 
         addPreferencesFromResource(R.xml.preference);
     }
-
 
 }
