9ddddd3174eb744c68c7730b613e0ee3484be888
Brian Huynh <brianhuynh1@gmail.com>
2016-04-06 17:28:57 -0700

    Clean up from da merge

--- GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/SettingsActivity.java
+++ GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/SettingsActivity.java
@@ -4,21 +4,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.preference.PreferenceManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class SettingsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
 
+        setContentView(R.layout.activity_settings);
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment()).commit();
     }
+
+
 
 }
