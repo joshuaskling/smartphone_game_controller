608a85e356dfb7f4fa5078a609d36edb982ab3ec
Traveling-Magic <sgardner@csumb.edu>
2016-04-08 13:48:08 -0700

    please wrok

--- GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/MainActivity.java
+++ GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/MainActivity.java
@@ -1,6 +1,12 @@
 package edu.csumb.gamecontroller;
 
 import android.app.Activity;
+<<<<<<< HEAD
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
+import android.graphics.Point;
+=======
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -9,7 +15,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -17,26 +22,39 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Vibrator;
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
 import android.preference.PreferenceManager;
+=======
+>>>>>>> parent of 4ad417e... Preference listener done
 import android.provider.Settings;
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
 import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+<<<<<<< HEAD
+=======
 import android.widget.Button;
 import android.widget.EdgeEffect;
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RelativeLayout;
+<<<<<<< HEAD
+=======
 import android.widget.Toast;
 //import android.support.v4.app.DialogFragment;
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
 
 import org.json.JSONObject;
 
@@ -48,25 +66,31 @@ public class MainActivity extends Activity implements SensorEventListener {
     static long time = System.currentTimeMillis();
     //long positionTimer = System.currentTimeMillis();
     public static Vibrator vibrator;
+<<<<<<< HEAD
+
+    public final String DEBUGMSG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+=======
     public LinearLayout mLinearLayout;
 
     public final String DEBUGMSG = "MainActivity";
-    public final String KEY_PREF_BTN_SIZE = "button_size_preference";
-    public final String KEY_PREF_BTN_COLOR = "button_color_preference";
-    public final String KEY_PREF_BACKGROUND = "background_preference";
-    public final String KEY_PREF_VOLUME = "volume_preference";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mLinearLayout = new LinearLayout(this);
 
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
         vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
 
         //set orientation to landscape
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
@@ -91,6 +115,9 @@ public class MainActivity extends Activity implements SensorEventListener {
 
 
 
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
+=======
+>>>>>>> parent of 4ad417e... Preference listener done
         //get size of screen
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
@@ -101,22 +128,67 @@ public class MainActivity extends Activity implements SensorEventListener {
         // Connection to server
         //connect();
 
+<<<<<<< HEAD
+        setContentView(R.layout.activity_main);
+
+=======
         // Attempt to draw EdgeEffect on view
         //setContentView(new DrawDemo(this));
         setContentView(R.layout.activity_main);
 
         // Controller buttons
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
         final ImageButton button1 = (ImageButton) findViewById(R.id.button1);
         final ImageButton button2 = (ImageButton) findViewById(R.id.button2);
         final ImageButton button3 = (ImageButton) findViewById(R.id.button3);
         final ImageButton button4 = (ImageButton) findViewById(R.id.button4);
 
+<<<<<<< HEAD
+        //radio buttons
+//        final RadioButton radio1 = (RadioButton) findViewById(R.id.radioButton1);
+//        final RadioButton radio2 = (RadioButton) findViewById(R.id.radioButton2);
+//        final RadioButton radio3 = (RadioButton) findViewById(R.id.radioButton3);
+
+        //setContentView(R.layout.main);
+        final RelativeLayout textView = (RelativeLayout)findViewById(R.id.joystickLayout);
+        // this is the view on which you will listen for touch events
+        final View touchView = findViewById(R.id.joystickLayout);
+
+        final RelativeLayout bgElement = (RelativeLayout) findViewById(R.id.background);
+
+        //radio controls
+        /*
+        radio1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bgElement.setBackgroundResource(R.drawable.nes_controller);
+            }
+        });
+
+        radio2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bgElement.setBackgroundResource(R.drawable.xbox);
+            }
+        });
+
+
+        radio3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bgElement.setBackgroundResource(R.drawable.wood_texture);
+            }
+        });
+        */
+
+=======
         final Button settingsBtn = (Button) findViewById(R.id.settingsBtn);
         final Button startBtn = (Button) findViewById(R.id.startBtn);
         final Button selectBtn = (Button) findViewById(R.id.selectBtn);
 
         final View touchView = findViewById(R.id.joystickLayout);
 
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
         //joystick controls
         touchView.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -124,8 +196,17 @@ public class MainActivity extends Activity implements SensorEventListener {
 
                 if (System.currentTimeMillis() - time > 100) {
 
+<<<<<<< HEAD
+                    //textView.setText("Touch coordinates : " +
+                    //        String.valueOf(event.getX()) + "x" + String.valueOf(event.getY()));
+                    //float x = (event.getRawX() - 250);
+                    //float y = (event.getRawY() - 250);
+                    float x = (event.getRawX()-(screenWidth/5));
+                    float y = (event.getRawY()-(screenHeight/2))*-1;
+=======
                     float x = (event.getRawX() - (screenWidth / 5));
                     float y = (event.getRawY() - (screenHeight / 2)) * -1;
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
 
                     Log.d(DEBUGMSG, "(" + Float.toString(x) + "," + Float.toString(y) + ")");
                     sendMovement(x, y);
@@ -135,6 +216,8 @@ public class MainActivity extends Activity implements SensorEventListener {
             }
         });
 
+<<<<<<< HEAD
+=======
 
 
         startBtn.setOnTouchListener(new View.OnTouchListener() {
@@ -153,6 +236,7 @@ public class MainActivity extends Activity implements SensorEventListener {
             }
         });
 
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
         button1.setOnTouchListener(
                 new View.OnTouchListener(){
                     @Override
@@ -261,6 +345,11 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
 
+<<<<<<< HEAD
+    //stick listeners
+
+
+=======
     public void loadSettings(View view) {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
@@ -294,6 +383,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 
 
     //stick listeners
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
     @Override
     protected void onResume() {
         super.onResume();
@@ -316,9 +406,27 @@ public class MainActivity extends Activity implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
 
+<<<<<<< HEAD
+        Log.d(DEBUGMSG, event.toString());
+        //position resources COMMENT OUT FOR SENSOR CHANGES
+        /*
+        if (System.currentTimeMillis()-positionTimer > 200) {
+            sendOrientation(event.values[0], event.values[1], event.values[2]);
+            // get the angle around the z-axis rotated
+            positionTimer = System.currentTimeMillis();
+        }
+        */
+
+        /*if (System.currentTimeMillis()-time > 500){
+            send2dMovement("stop");
+        }*/
+    }
+
+=======
     }
 
 
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // not in use
@@ -327,11 +435,34 @@ public class MainActivity extends Activity implements SensorEventListener {
     void sendFire(String input, String state) {
         // get the angle around the z-axis rotated
         Log.d(DEBUGMSG, "Input: " + input + ", State: " + state);
+<<<<<<< HEAD
+        /*
+        try {
+
+            JSONObject message = new JSONObject(new String("{state: " + state + "}"));
+
+            if (socket.isConnected()) {
+                socket.emit(input, message);
+                //Log.i("AIPSERVER", "Message sent to server: fire!");
+                System.out.println(input);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+
+    }
+=======
 
     }
 
+<<<<<<< HEAD
 
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
 
+=======
+>>>>>>> parent of 4ad417e... Preference listener done
     void sendOrientation(float deltaAlpha, float deltaBeta, float deltaGamma) {
 
 
@@ -370,7 +501,67 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     void sendMovement(float x, float y) {
+<<<<<<< HEAD
+        /*
+        try {
+            String messageContent = new String("{x: "+x+ ",y: " + y + "}");
+
+            JSONObject message = new JSONObject(messageContent);
+
+            if (socket.isConnected()) {
+                socket.emit("joystick", message);
+                //Log.i("AIPSERVER", "Message sent to server: " + message.getString("movement"));
+                System.out.println("X: " + x + " Y: " + y);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
 
     }
+    /*
+    void connect() {
+
+        socket = new IOSocket("http://aipservers.com:3000", new MessageCallback() {
+
+            @Override
+            public void onMessage(String message) {
+                // Handle simple messages
+            }
+
+            @Override
+            public void onConnect() {
+                // Socket connection opened
+            }
+
+            @Override
+            public void onDisconnect() {
+                // Socket connection closed
+            }
+
+            @Override
+            public void on(String event, JSONObject... data) {
+
+            }
+
+            @Override
+            public void onMessage(JSONObject json) {
+
+            }
+
+            @Override
+            public void onConnectFailure() {
+
+            }
+        });
+
+        socket.connect();
+    }
+    */
+=======
+
+    }
+>>>>>>> 885b9c0c8f74af190a459dd1ff5abb1bc01cbe73
 
 }
