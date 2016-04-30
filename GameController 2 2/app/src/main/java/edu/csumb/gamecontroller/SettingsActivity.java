32b37e9ef60d04d96aa5593f7eab232cf1598db5
Brian Huynh <brianhuynh1@gmail.com>
2016-04-29 17:50:23 -0700

    Cleaned up

--- GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/SettingsActivity.java
+++ GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/SettingsActivity.java
@@ -1,19 +1,8 @@
 package edu.csumb.gamecontroller;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.preference.PreferenceManager;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.widget.Toolbar;
 
 public class SettingsActivity extends PreferenceActivity {
 
