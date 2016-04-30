01ef4e7646051b289110c424693e9f2685168e42
Traveling-Magic <sgardner@csumb.edu>
2016-04-29 18:30:14 -0700

    Re-inserted GameController

---
+++ GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/MessageCallback.java
@@ -0,0 +1,12 @@
+package edu.csumb.gamecontroller;
+
+import org.json.JSONObject;
+
+public interface MessageCallback {
+	public void on(String event, JSONObject... data);
+	public void onMessage(String message);
+	public void onMessage(JSONObject json);
+	public void onConnect();
+	public void onDisconnect();
+	public void onConnectFailure();
+}
